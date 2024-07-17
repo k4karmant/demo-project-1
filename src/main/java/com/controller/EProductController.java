8fc949fdbc3e5bb288d651c309dd41bd66496aab
k4karmant <k4karmant@gmail.com>
2024-07-17 19:18:13 +0530

    Add to cart

---
+++ src/main/java/com/controller/EProductController.java
@@ -0,0 +1,84 @@
+package com.controller;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bean.EProductBean;
+import com.dao.EProductDao;
+import com.service.FileUploadService;
+
+@Controller
+public class EProductController {
+
+	// @Autowired
+	// AwsS3Service s3;
+
+	@Autowired
+	EProductDao productDao;
+	
+	@Autowired
+	FileUploadService fileUploadService;
+
+	@GetMapping("/newproduct") // url->browser
+	public String newProduct() {// method name
+		return "NewProduct";// jsp name
+	}
+
+	// int a = 10
+	@PostMapping("/saveproduct")
+	public String saveProduct(EProductBean productBean, @RequestParam("masterImage") MultipartFile masterImage) 
+	{	
+		
+		fileUploadService.uploadProductImage(masterImage);
+		productBean.setProductImagePath("images//products//"+ masterImage.getOriginalFilename());
+		productDao.addProduct(productBean);
+
+		return "redirect:/products";
+	}
+
+	@GetMapping("/products")
+	public String listProducts(Model model) {
+		List<EProductBean> products = productDao.getAllProducts();// return type
+
+		model.addAttribute("products", products);
+
+		return "EcomListProducts";
+	}
+
+	@GetMapping("/deleteproduct")
+	public String deleteProduct(@RequestParam("productId") Integer productId) {
+
+		System.out.println("deleteProduct() => " + productId);
+
+		productDao.deleteProduct(productId);
+		return "redirect:/products";// do not open jsp
+		// open an url
+
+	}
+
+	// form ->data -> read -> bean
+
+	// hyperlink -> ? -> query string -> @RequestParam("q") datatyep variablename
+	// @RequestParam("productId") Integer productId
+
+	@GetMapping("/viewproduct")
+	public String viewProduct(@RequestParam("productId") Integer productId, Model model) {
+
+		// id->details->table : products
+		// select * from products where productId = ?
+		EProductBean productBean = productDao.getProductById(productId);
+		model.addAttribute("product", productBean);
+
+		return "ViewProduct";
+	}
+
+}
---
+++ src/main/java/com/controller/EcomSessionController.java
@@ -0,0 +1,90 @@
+package com.controller;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.EUserBean;
+import com.dao.EUserDao;
+import com.service.FileUploadService;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EcomSessionController {
+
+	@Autowired
+	EUserDao userDao;
+
+	@Autowired
+	FileUploadService fileUploadService;
+
+	@GetMapping(value = { "/", "elogin" })
+	public String welcome() {
+		return "EcomLogin";
+	}
+
+	// authentication
+	@PostMapping("/elogin")
+	public String eLogin(EUserBean userBean, Model model, HttpSession session) {
+		System.out.println("32 => " + userBean.getEmail());
+		System.out.println("33 => " + userBean.getPassword());
+
+		// a
+		// b
+		// select * from users where email = ? and password = ?
+		EUserBean dbUser = userDao.authenticate(userBean.getEmail(), userBean.getPassword());
+		if (dbUser == null) {
+			model.addAttribute("error", "Invalid Credentials");
+			return "EcomLogin";
+		} else {
+
+			session.setAttribute("user", dbUser);
+			model.addAttribute("firstName", dbUser.getFirstName());
+			model.addAttribute("profilePicPath", dbUser.getProfilePicPath());
+			return "EcomHome";
+		}
+	}
+
+	@GetMapping("/esignup")
+	public String signup() {
+		return "EcomSignup";
+	}
+
+	@PostMapping("/esignup")
+	public String signupPost(EUserBean userBean) {
+		// jsp form -> controller ->
+		// validation
+
+		fileUploadService.uploadUserImage(userBean.getProfilePic(), userBean.getEmail());
+
+		// images//profilepic//rock1@gmail.com//123.png
+
+		userBean.setProfilePicPath("images//profilepic//" + userBean.getEmail() + "//" + userBean.getProfilePic().getOriginalFilename());
+		// insert
+		userDao.insertUser(userBean);
+		// image server upload
+		// id
+
+		// profilepic/jack@gmai.com/timestamp_aa.jpg
+
+		// profilepic/0522461526122aa.jpg
+		// profilepic/0522461527122aa.jpg
+
+		return "EcomLogin";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		//destroy session 
+		session.invalidate();
+		return "redirect:/elogin";
+	}
+
+}
